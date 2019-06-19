09e6ca6c5f0f80915b996be0ae33015c36b65846
Eric <chiholau@conncect.hku.hk>
2019-06-20 06:14:03 +0800

    update deploy script

--- deploy/deployUpdates.sh
+++ deploy/deployUpdates.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yarn --cwd /home/ubuntu/peer-learning stop
-git -C /home/ubuntu/peer-learning pull
+ssh-agent bash -c 'ssh-add /home/ubuntu/.ssh/id_rsa; git -C /home/ubuntu/peer-learning pull'
 sudo rm -rf /home/ubuntu/peer-learning/data /home/ubuntu/peer-learning/repo
 sudo yarn --cwd /home/ubuntu/peer-learning run build
 sudo yarn --cwd /home/ubuntu/peer-learning start
